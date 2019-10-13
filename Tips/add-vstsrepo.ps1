fb8099cf7b21aa27169e8100f316ec31902ec1fe
John J Kavanagh <John.kavanagh@kavanaghtech.net>
2019-10-13 11:46:00 -0400

    Weg: Git install
    Updating code format for PSSA
    
    TODO# Add install routine.

--- Tips/add-vstsrepo.ps1
+++ Tips/add-vstsrepo.ps1
@@ -1,13 +1,14 @@
 Install-Package -Name git -ProviderName Chocolatey
+# Install from C:\Chocolatey\lib\git.install.2.23.0\tools
 set-location $env:USERPROFILE\Documents
 if (test-path github){
-    set-location github
+	set-location github
 }
 Else {
-    New-Item -Name github -Path $env:USERPROFILE\Documents -ItemType Directory
-    set-location github
+	New-Item -Name github -Path $env:USERPROFILE\Documents -ItemType Directory
+	set-location github
 }
-    $genWarning = "Demonstration of ScriptAnalyzer in VSCode - your ISE Can't do that"
+	$genWarning = "Demonstration of ScriptAnalyzer in VSCode - your ISE Can't do that"
 git clone https://wegit.visualstudio.com/ServiceDelivery/Automation/_git/ServiceDelivery
 
 
